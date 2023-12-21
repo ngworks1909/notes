2d3b15e86f1f4b83199dd337cc80be14f78273e4
Nithin1908 <76480955+ngworks1909@users.noreply.github.com>
2023-12-21 13:29:08 +0530

    first commit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
       e.preventDefault();
       const email = e.target[0].value;
       const password = e.target[1].value;
-      const response = await fetch(`http://localhost:3001/api/auth/login`, {
+      const response = await fetch(`https://notebook-jrbok9nj5-nithins-projects-d106c539.vercel.app/api/auth/login`, {
               method: "POST",
               headers: {
                   "Content-Type": "application/json",
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -17,7 +17,7 @@ export default function Register({setLogin}) {
         const name = e.target[0].value;
         const email = e.target[1].value;
         const password = e.target[2].value;
-        const response = await fetch(`http://localhost:3001/api/auth/createUser`, {
+        const response = await fetch(`https://notebook-jrbok9nj5-nithins-projects-d106c539.vercel.app/api/auth/createUser`, {
         method: "POST",
         headers: {
               "Content-Type": "application/json",

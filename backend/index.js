0d32b903f0fc4fe196809a5030e6df43065a3842
Nithin1908 <76480955+ngworks1909@users.noreply.github.com>
2024-03-08 21:14:59 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,12 @@ connectToMongo();
 //connected to mongo
 const app = express();
 const port = 3001;
-app.use(cors())
+app.use(
+    cors({
+      origin: ["http://localhost:3001","https://notes-six-mu.vercel.app/"],
+      credentials: true,
+    })
+  );
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
